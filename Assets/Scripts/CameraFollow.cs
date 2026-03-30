2e46ab7ab47349d67b26dacdba26052b992fc67b
Huseyn Jafarli <hjeferli18088@ada.edu.az>
2026-03-30 23:34:20 +0400

    2 new enemies added, level design completed and tested.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform player;
-    Vector3 offset = new Vector3(0, 3, -6);
+    public Vector3 offset = new Vector3(0, 4, -6);
+    public float lookOffset = 2.0f; 
 
     void LateUpdate()
     {
         transform.position = player.position + offset;
-        transform.LookAt(player);
+
+        Vector3 lookTarget = player.position + Vector3.up * lookOffset;
+        transform.LookAt(lookTarget);
     }
 }
